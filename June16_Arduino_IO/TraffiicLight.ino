95e524c43495c14503a83c5a67666f879b14e861
Zheki <47313913+Zheki@users.noreply.github.com>
2020-06-16 13:15:53 +0400

    Update TraffiicLight.ino

--- June16_Arduino_IO/TraffiicLight.ino
+++ June16_Arduino_IO/TraffiicLight.ino
@@ -1,17 +1,17 @@
-//Zheki
+//Zheki 16.06.2020
 
-const int Potentiometer = A0;
-const int PhotoResistor = A1;
+//Declaring variables for:
+const int Potentiometer = A0;  //Potentiometer connected to analog A0 pin
+const int PhotoResistor = A1;  //Photo resistor connected to analog A1 pin
 const int outPin_1 = 9;
-const int outPin_2 = 10;
+const int outPin_2 = 10;       //The three digital output pins to which the LEDs are conneceted
 const int outPin_3 = 11;
 
-int potValue = 0;        
-//int outputValue = 0;        
+int potValue = 0;              //Declaring the variables for the sensors' values.
 int photoResValue = 0;
 
 void setup() {
-  Serial.begin(250000);
+  Serial.begin(250000);        //High com rate for smoother LED glow.
 }
 
 void loop() {
@@ -20,8 +20,8 @@ void loop() {
   photoResValue = analogRead(PhotoResistor);
 
   if (potValue < 340) {
-    analogWrite(outPin_1, 255);
-  }
+    analogWrite(outPin_1, 255);                 //IF conditionals to check the value of the potentiometer
+  }                                             //and give the appropriate output.
   else {
     analogWrite(outPin_1, 0);
   }
@@ -38,15 +38,9 @@ void loop() {
     analogWrite(outPin_3, 0);
   }
 
-
-  Serial.print("sensor = ");
-  Serial.print(potValue);
-  Serial.print("\t output = ");
-  Serial.println(photoResValue);
-
   if (photoResValue < 50) {
     digitalWrite(outPin_1, HIGH);
-    digitalWrite(outPin_2, HIGH);
+    digitalWrite(outPin_2, HIGH);              //If conditional which checks if the photo resistor value is below a the specified threshold.
     digitalWrite(outPin_3, HIGH);
   }
 
